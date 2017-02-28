3f380815915131c9d519e6dd692b6c9d10e3d42f
Vess Bakalov <vess@virgoscorpius.com>
2017-02-28 15:07:29 -0500

    she is alive `

--- app.js
+++ app.js
@@ -20,11 +20,33 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 //Console.log("bitch");
 
+var model = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/3bcc6ee2-16cf-4a0d-af93-61affd3d38d7?subscription-key=bbfc8b820c87430e82839b055cfc35e1&verbose=true";
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
 //=========================================================
 // Bots Dialogs
-//=========================================================
-
+//=======================================fe==================
+/*
 bot.dialog('/', function (session) {
     session.send("Hello World");
     console.log("what\n");
 });
+*/
+
+bot.dialog('/', dialog);
+console.log( "got it" );
+dialog.matches('whoIs', [
+    function( session, args, next ) {
+        var name = builder.EntityRecognizer.findEntity( args.entities, 'name::firstName' );
+        console.log("I am here "  + name.entity );
+        session.send("How am I supposed to know who " + name.entity + " is?" );
+    } 
+]);
+    
+  
+
+
+
+//dialog.matches('scheduleMeeting', builder.DialogAction.send("hey"));
+dialog.onDefault( builder.DialogAction.send('I cant hear you'));
